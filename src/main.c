81890f6f8a6e2271ab7d559a36bec4f47c5d34a7
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2016-09-01 20:35:03 +0530

    Use stat now, using shell commands are for noobs

--- src/main.c
+++ src/main.c
@@ -1,26 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-
+#include <grp.h>
+#include <pwd.h>
+#include <sys/stat.h>
 #define MAX_LENGTH 255
 
 int main(int argc, char *argv[])
 {
 	if (argc>=2)
 	{
-
-		int i=0;
-		char *file_path=argv[1];
-		char *command = "stat -c \"%n: %U-%G\" ";
-		char *tmp = calloc(sizeof(file_path)+sizeof(command),sizeof(char));
-		while(*command){
-      		tmp[i++]=*command++;
-  		}
-  		while(*file_path){
-      		tmp[i++]=*file_path++;
-  		}
-  		tmp[i]='\0';
-		system(tmp);
+		int i;
+		printf("  FILENAME\t     OWNER      GROUP\n");
+		for(i=1;i<argc;i++) {
+			struct stat info;
+			char *file_path=argv[i];
+			stat(file_path,&info);
+			struct passwd *pw = getpwuid(info.st_uid);
+			struct group  *gr = getgrgid(info.st_gid);			
+			printf("%10s\t%10s  %10s\n", file_path,pw->pw_name,gr->gr_name);
+		}
 
 	} else {
 		printf("Who's Your Daddy: A commandline tool to find the owner:group of a file/folder.\nUsage:\twhosyodaddy <file path>\n");
