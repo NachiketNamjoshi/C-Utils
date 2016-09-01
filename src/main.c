64289bf0449b757b753f0634d9a9be200fd6b963
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2016-09-01 07:46:57 +0530

    Add Usage and Check for arguments to prevent Segmentation fault

--- src/main.c
+++ src/main.c
@@ -8,15 +8,20 @@ int main(int argc, char *argv[])
 {
 	int status=0,i=0;
 	char *file_path=argv[1];
-	char *command = "stat -c \"%U %G\" ";
-	char *tmp = calloc(sizeof(file_path)+sizeof(command),sizeof(char));
-	while(*command){
-      tmp[i++]=*command++;
-  }
-  while(*file_path){
-      tmp[i++]=*file_path++;
-  }
-  tmp[i]='\0';
-	system(tmp);
+	if (argc==2)
+	{
+		char *command = "stat -c \"%U %G\" ";
+		char *tmp = calloc(sizeof(file_path)+sizeof(command),sizeof(char));
+		while(*command){
+      		tmp[i++]=*command++;
+  		}
+  		while(*file_path){
+      		tmp[i++]=*file_path++;
+  		}
+  		tmp[i]='\0';
+		system(tmp);
+	} else {
+		printf("Who's Your Daddy: A commandline tool to find the owner:group of a file/folder.\nUsage:\twhosyodaddy <file path>\n");
+	}
 	return 0;
 }
