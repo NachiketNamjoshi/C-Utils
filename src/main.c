206fa3edbaeb23c2209852ee7435ed334637db84
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2016-09-01 07:28:08 +0530

    Fix Warnings and Segmentation Fault

--- src/main.c
+++ src/main.c
@@ -8,14 +8,10 @@ int main()
 {
 	int status=0,i=0;
 	char *file_path=malloc(sizeof(char)*MAX_LENGTH);
-	printf("%f\n",sizeof(file_path));
-	char *command = 'stat -c "%U %G" ';
-	printf("%f\n", sizeof(command));
+	char *command = "stat -c \"%U %G\" ";
 	printf("Enter Path to file: \n");
 	scanf("%s", file_path);
-	printf("Path to file: %s\n",file_path);
-	char *tmp = malloc(sizeof(file_path)+sizeof(command));
-	printf("%f\n", sizeof(tmp));
+	char *tmp = calloc(sizeof(file_path)+sizeof(command),sizeof(char));
 	while(*command){
       tmp[i++]=*command++;
   }
@@ -23,7 +19,6 @@ int main()
       tmp[i++]=*file_path++;
   }
   tmp[i]='\0';
-	printf("command: %s\n",tmp);
-	system(command);
+	system(tmp);
 	return 0;
 }
