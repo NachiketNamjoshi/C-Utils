f773685993d6899439fa3db5dfd5b0e60db3d74b
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2016-09-04 14:43:38 +0530

    cryptomessenger: Create Skeleton

---
+++ src/cryptomessenger/crypt_utils.h
@@ -0,0 +1,18 @@
+#ifndef __CRYPT_UTILS_H__
+#define __CRYPT_UTILS_H__
+
+#define MAX_LENGTH 256
+
+char* crypt(char[] cipheredText, char[] message) {
+
+
+	return cipheredText;
+}
+
+char *decrypt(char[] cipheredText, char[] message) {
+
+
+	return message;
+}
+
+#endif
---
+++ src/cryptomessenger/cryptomessenger.c
@@ -0,0 +1,8 @@
+#include "crypt_utils.h"
+
+int main() {
+
+
+
+	return 0;
+}
